b8f33a9c9dbeefe33dfb581d70e387275df3a073
dtello98 <diegoalonsotelloalva1998@gmail.com>
2025-12-08 23:03:07 -0500

    avance de clase

---
+++ clase_4/2-tiposdatos.py
@@ -0,0 +1,13 @@
+# Crearemos un programa que sume 2 numeros
+# El codigo lo dividimos en 3 partes
+
+# 1.Datos de entrada
+# 2.Proceso
+# 3.Datos de Salida
+
+numero1 = input("Ingrese el primer numero")
+numero2 = input("Ingrese el primer numero")
+
+resultado=numero1+numero2
+
+print("El resultado es ", resultado)
